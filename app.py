0e54019cfdd1522a96d0b3e72ee14f4a9a83c9a4
Fabshkiee <pwedefabros@gmail.com>
2025-04-02 11:22:43 +0800

    Refactor authentication credentials path and update dashboard template; changed path for index.html and style.css files.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from google.oauth2.service_account import Credentials
 app = Flask(__name__)
 app.secret_key = 'your-secret-key-here'
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file(f"/storage/emulated/0/UOV HACK/upvhackathonCreds.json", scopes=scopes)
+creds = Credentials.from_service_account_file("upvhackathonCreds.json", scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet_id = "15P43fHag6Va8upWyhvUJwV0ECbtU4zeMsFp5DiPUXzM"
@@ -40,7 +40,7 @@ def login():
 def dashboard():
     if 'user_id' not in session:
         return redirect ('/')
-    return render_template('pretest.html')
+    return render_template('landingpage.html')
             
     
     
