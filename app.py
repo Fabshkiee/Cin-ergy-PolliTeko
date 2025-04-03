562957deccce73c773ea43bb0d3e8445c3223252
Fabshkiee <pwedefabros@gmail.com>
2025-04-03 12:06:14 +0800

    Update file paths, enhance login styles, and add question template

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from google.oauth2.service_account import Credentials
 app = Flask(__name__)
 app.secret_key = 'your-secret-key-here'
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("upvhackathonCreds.json", scopes=scopes)
+creds = Credentials.from_service_account_file("Cin-ergy-PolliTeko/upvhackathonCreds.json", scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet_id = "15P43fHag6Va8upWyhvUJwV0ECbtU4zeMsFp5DiPUXzM"
