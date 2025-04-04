3fba14bc1ea163c89a19f9fd69bae31a505a0bfc
GemAdrian-WVSU <gemadrian.candaganan@wvsu.edu.ph>
2025-04-04 16:59:31 +0800

    Add HTML forms for candidate and pillar management, enhance navigation, and implement dynamic position listing

---
+++ BACK-END/backend.js
@@ -0,0 +1,15 @@
+document.getElementById("addCandidate").onclick = function () {
+    window.location.href = "addCandidate.html";
+}
+
+document.getElementById("addPillar").onclick = function () {
+    window.location.href = "addPillar.html";
+}
+
+document.getElementById("createQuestion").onclick = function () {
+    window.location.href = "createQuestions.html";
+}
+
+document.getElementById("listPosition").onclick = function () {
+    window.location.href = "listposition.html";
+}
